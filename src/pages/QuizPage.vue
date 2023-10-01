7c1ca8d0e0f239d126fe5153b50306da2c4ade9b
Dan Ryland <dan@ryland.consulting>
2023-10-01 23:49:22 +0100

    add better quiz loop

--- src/pages/QuizPage.vue
+++ src/pages/QuizPage.vue
@@ -1,5 +1,12 @@
 <template>
   <q-page class="page-quiz">
+    <q-linear-progress
+      rounded
+      size="12px"
+      :value="quizProgress"
+      color="green"
+      class="progress"
+    />
     <h1>{{ activeQuestionItem.text }}</h1>
     <q-list :class="{ 'show-answer': showAnswer === true }">
       <template
@@ -37,26 +44,43 @@
       color="primary"
       rounded
       no-caps
-      label="Check"
+      label="Check answer"
       @click="checkAnswer()"
     />
     <q-btn
-      v-if="answers[activeQuestionIndex] != null && showAnswer"
+      v-if="
+        answers[activeQuestionIndex] != null &&
+        showAnswer &&
+        activeQuestionIndex + 1 != totalSteps
+      "
       class="btn-large full-width"
       color="green"
       rounded
       no-caps
-      label="Next"
+      label="Next question"
       @click="nextQuestion()"
+    />
+    <q-btn
+      v-if="
+        answers[activeQuestionIndex] != null &&
+        showAnswer &&
+        activeQuestionIndex + 1 == totalSteps
+      "
+      class="btn-large full-width"
+      color="primary"
+      rounded
+      no-caps
+      label="Done"
+      :to="{ name: 'learn' }"
     />
   </q-page>
   <q-footer class="footer-quiz">
     <q-btn
-      class="btn-large full-width"
+      class="full-width"
       flat
       rounded
       no-caps
-      label="Learn factors"
+      label="Learn about factors"
       :to="{ name: 'learn' }"
     />
   </q-footer>
@@ -77,12 +101,27 @@ export default {
     let answers: Ref<(number | null)[]> = ref(new Array(15).fill(null));
     const questions = <Question[]>[
       {
+        text: 'Select the prime number',
+        options: ['3', '4', '20'],
+        answer: 0,
+      },
+      {
+        text: 'Select the composite number',
+        options: ['2', '4', '5'],
+        answer: 1,
+      },
+      {
         text: 'What are the prime factors of 42',
         options: ['3 × 14', '2 × 3 × 7', '2 × 21'],
         answer: 1,
       },
       {
         text: 'What are the prime factors of 150?',
+        options: ['13', '3', '39'],
+        answer: 0,
+      },
+      {
+        text: 'What is the LCM of 13 and 3?',
         options: ['2 × 3 × 5 × 5', '3 × 3 × 5 × 5', '6 × 25'],
         answer: 0,
       },
@@ -108,6 +147,10 @@ export default {
       }
     };
 
+    const totalSteps = ref(questions.length);
+    const activeStep = computed(() => activeQuestionIndex.value + 1);
+    const quizProgress = computed(() => activeStep.value / totalSteps.value);
+
     const nextQuestion = () => {
       activeQuestionIndex.value += 1;
       reset();
@@ -129,6 +172,8 @@ export default {
       isAnswerCorrect,
       nextQuestion,
       reset,
+      quizProgress,
+      totalSteps,
     };
   },
 };
